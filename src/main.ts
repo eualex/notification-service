c5aed1069e8877243b4be93529a572773935a6c4
alex.matos <alex.matos@ceuma.br>
2022-12-16 11:44:38 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
@@ -6,6 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.useGlobalPipes(new ValidationPipe())
+
+  const config = new DocumentBuilder()
+    .setTitle('Notifications Service')
+    .setDescription('A microservice to send and manage notifications')
+    .setVersion('1.0')
+    .addTag('notifications')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
 
   await app.listen(3000)
 }
